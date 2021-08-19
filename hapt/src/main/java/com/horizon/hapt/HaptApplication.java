423e416e455de3881bfafee0c42a4d6c4ba84cfd
soyasou <souadaloui104@gmail.com>
2021-08-19 18:21:40 +0200

    add project java spring

---
+++ hapt/src/main/java/com/horizon/hapt/HaptApplication.java
@@ -0,0 +1,13 @@
+package com.horizon.hapt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaptApplication.class, args);
+	}
+
+}
