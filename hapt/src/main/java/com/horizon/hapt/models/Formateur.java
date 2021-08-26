2a0a3a50a112c2875a3d88af019122974145105a
soyasou <souadaloui104@gmail.com>
2021-08-26 17:14:58 +0200

    add class

--- hapt/src/main/java/com/horizon/hapt/models/Formateur.java
+++ hapt/src/main/java/com/horizon/hapt/models/Formateur.java
@@ -1,5 +1,48 @@
 package com.horizon.hapt.models;
 
 public class Formateur {
+    private String nom;
+    private String prenom;
+    private int tel;
+    private String adress;
+
+    public Formateur(String nom, String prenom, int tel, String adress) {
+        this.setNom(nom);
+        this.setPrenom(prenom);
+        this.setTel(tel);
+        this.setAdress(adress);
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public int getTel() {
+        return tel;
+    }
+
+    public void setTel(int tel) {
+        this.tel = tel;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
 
 }
